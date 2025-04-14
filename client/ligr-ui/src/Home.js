923077329eef59b2f66eeb09981ceb60c7bac04b
nathann07 <ntmebane@gmail.com>
2025-04-14 13:18:22 -0400

    added top 10 games list stub to homepage

--- client/ligr-ui/src/Home.js
+++ client/ligr-ui/src/Home.js
@@ -3,6 +3,24 @@ function Home() {
   return (
     <div>
       <h2>HomePage for Lorem Ipsum Game Reviews</h2>
+    <section>
+      <h3 className="mb-4">Top 10 Games</h3>
+      <table className="table table-striped table-hover">
+        <thead className="thead-dark">
+          <tr>
+            <th>Name</th>
+            <th>Developer - Producer</th>
+            <th>Year Released</th>
+            <th>Top Sale Region</th>
+            <th>Rating</th>
+            <th>&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* populate table with map function */}
+        </tbody>
+      </table>
+    </section>
     </div>
   );
 }
