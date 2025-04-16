87af950ce964b953c791dd360b0648b2fd569bf0
nathann07 <ntmebane@gmail.com>
2025-04-16 13:18:16 -0400

    fixed register and login, started homepage. fixed notblank to notnull in Game model

--- client/ligr-ui/src/Home.js
+++ client/ligr-ui/src/Home.js
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [games, setGames] = useState([]);
+  const url = "http://localhost:8080/game";
+
+  // useEffect fetches data when component mounts
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          return Promise.reject(`Unexpected Status Code: ${response.status}`);
+        }
+      })
+      .then((data) => setGames(data))
+      .catch(console.log);
+  }, []);
+
   return (
     <div>
       <h2>HomePage for Lorem Ipsum Game Reviews</h2>
@@ -9,7 +27,7 @@ function Home() {
         <thead className="thead-dark">
           <tr>
             <th>Name</th>
-            <th>Developer - Producer</th>
+            <th>Developer</th>
             <th>Year Released</th>
             <th>Top Sale Region</th>
             <th>Rating</th>
@@ -17,7 +35,14 @@ function Home() {
           </tr>
         </thead>
         <tbody>
-          {/* populate table with map function */}
+          {games.map((game) => (
+              <tr key={game.gameId}>
+                <td>{game.title}</td>
+                <td>{game.developer}</td>
+                <td>{game.yearReleased}</td>
+                <td>{game.region}</td>
+             </tr>
+            ))}
         </tbody>
       </table>
     </section>
