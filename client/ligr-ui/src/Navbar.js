4bd3ba6f48261ddd96c193cc05d283c50649718d
nathann07 <ntmebane@gmail.com>
2025-04-16 12:27:21 -0400

    fixed urls in frontend

--- client/ligr-ui/src/Navbar.js
+++ client/ligr-ui/src/Navbar.js
@@ -4,8 +4,8 @@ function Navbar() {
   return (
     <nav>
       <Link to={"/"}>Home</Link>
-      <Link to={"/login"}>Login</Link>
-      <Link to={"/register"}>Register</Link>
+      <Link to={"/profile"}>Profile</Link>
+      <Link to={"/logout"}>Log Out</Link>
     </nav>
   );
 }
