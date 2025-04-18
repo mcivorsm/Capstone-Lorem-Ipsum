207d92cb5de14a542a4f04a2cc85604f2556ab37
nathann07 <ntmebane@gmail.com>
2025-04-18 01:14:01 -0400

    hopefully final background panel fix, for different screen sizes

--- client/ligr-ui/src/App.js
+++ client/ligr-ui/src/App.js
@@ -64,6 +64,8 @@ function App() {
             margin: "2rem auto",
             padding: "2rem",
             borderRadius: "10px",
+            minWidth: "1200px",
+            minHeight: "90vh",
             width: "fit-content",
             boxShadow: "0 0 15px rgba(0,0,0,0.2)",
           }}
